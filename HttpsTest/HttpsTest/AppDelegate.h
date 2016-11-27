d0a1cc25c5214d00a8df75ada2995af08b5b709c
action163 <543262853@qq.com>
2016-11-27 17:55:00 +0800

    add NSLog("hello world")

---
+++ HttpsTest/HttpsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HttpsTest
+//
+//  Created by jzl on 2016/11/27.
+//  Copyright © 2016年 jzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HttpsTest/HttpsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HttpsTest
+//
+//  Created by jzl on 2016/11/27.
+//  Copyright © 2016年 jzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
