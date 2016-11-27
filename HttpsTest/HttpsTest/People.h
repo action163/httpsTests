71eb7ca7e2b0c3ebded30f418dcce78fbc3d3238
action163 <543262853@qq.com>
2016-11-27 18:14:48 +0800

    add people class

---
+++ HttpsTest/HttpsTest/People.h
@@ -0,0 +1,13 @@
+//
+//  People.h
+//  HttpsTest
+//
+//  Created by jzl on 2016/11/27.
+//  Copyright © 2016年 jzl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+
+@end
---
+++ HttpsTest/HttpsTest/People.m
@@ -0,0 +1,15 @@
+//
+//  People.m
+//  HttpsTest
+//
+//  Created by jzl on 2016/11/27.
+//  Copyright © 2016年 jzl. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+-(void)hello{
+    NSLog(@"hello hello hello");
+}
+@end
